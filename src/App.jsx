749b5de37dba28219056629af891d0c6273fcc21
winifredogbeiwi <imadeogbeiwi@gmail.com>
2024-06-30 16:41:02 +0200

    #Setup: Routers set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import ResetPassword from './components/auth/ResetPassword'
+import { PATH } from './constants/util'
+import Dashboard from './components/dashboard/Dashboard'
+import Expenses from './components/dashboard/Expenses'
+import Income from './components/dashboard/Income'
+import Savings from './components/dashboard/Savings'
+import Settings from './components/dashboard/Settings'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Routes>
+        <Route path={PATH.DEFAULT} element={<Navigate to={PATH.LOGIN} replace />} />
+        <Route path={PATH.LOGIN} element={<Login />} />
+        <Route path={PATH.REGISTER} element={<Register />} />
+        <Route path={PATH.RESETPASSWORD} element={<ResetPassword />} />
+
+        {/* protected Path */}
+        <Route path={PATH.DASHBOARD} element={<Dashboard />} />
+        <Route path={PATH.EXPENSES} element={<Expenses />} />
+        <Route path={PATH.INCOME} element={<Income />} />
+        <Route path={PATH.SAVINGS} element={<Savings />} />
+        <Route path={PATH.SETTINGS} element={<Settings />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/constants/util.js
@@ -0,0 +1,14 @@
+export const PATH = {
+  DEFAULT: "/",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  RESETPASSWORD: "/reset-password",
+
+  //   protected path
+  DASHBOARD:"/dasboard",
+  INCOME:"/income",
+  EXPENSES:"/expenses",
+  SAVINGS:"/savings",
+  SETTINGS:"/settings"
+
+};
