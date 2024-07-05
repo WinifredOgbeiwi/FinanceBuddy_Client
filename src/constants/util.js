cfeec39aca835cb7cfa2b040443015ec48c63e0b
winifredogbeiwi <imadeogbeiwi@gmail.com>
2024-07-05 13:12:03 +0200

    Change Income icin

--- src/constants/util.js
+++ src/constants/util.js
@@ -1,10 +1,11 @@
-import Logo from '../assets/images/logo.png'
+import Logo from "../assets/images/logo.png";
 import LogoWhite from "../assets/images/logo_white.png";
 import BgCircle from "../assets/images/bgcircle.png";
 import ProfilePic from "../assets/images/sample_pic.jpg";
 import Loader from "../assets/images/loaderLogo.gif";
-import { MdOutlineDashboardCustomize } from 'react-icons/md';
-import { BsCashCoin } from 'react-icons/bs';
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { BsCashCoin } from "react-icons/bs";
+import { GiReceiveMoney } from "react-icons/gi";
 export const PATH = {
   DEFAULT: "/",
   LOGIN: "/login",
@@ -12,12 +13,11 @@ export const PATH = {
   RESETPASSWORD: "/reset-password",
 
   //   protected path
-  DASHBOARD:"/dashboard",
-  INCOME:"/income",
-  EXPENSES:"/expenses",
-  SAVINGS:"/savings",
-  SETTINGS:"/settings"
-
+  DASHBOARD: "/dashboard",
+  INCOME: "/income",
+  EXPENSES: "/expenses",
+  SAVINGS: "/savings",
+  SETTINGS: "/settings",
 };
 
 export const IMAGES = {
@@ -28,43 +28,40 @@ export const IMAGES = {
   ProfilePic,
 };
 
+export const passwordConditions = [
+  { regex: /.{8,}/, message: "Must be at least 8 characters" },
+  { regex: /\d/, message: "Must contain one number" },
+  { regex: /[A-Z]/, message: "Must contain one alphabet in uppercase" },
+  { regex: /[a-z]/, message: "Must contain one alphabet in lowercase" },
+  {
+    regex: /[!@#$%^&*]/,
+    message: "Must contain one special character (@, #, &, % etc)",
+  },
+];
 
-  export const passwordConditions = [
-    { regex: /.{8,}/, message: "Must be at least 8 characters" },
-    { regex: /\d/, message: "Must contain one number" },
-    { regex: /[A-Z]/, message: "Must contain one alphabet in uppercase" },
-    { regex: /[a-z]/, message: "Must contain one alphabet in lowercase" },
-    {
-      regex: /[!@#$%^&*]/,
-      message: "Must contain one special character (@, #, &, % etc)",
-    },
-  ];
-
-
-
-  export const sidebar = [
-    {
-      id: 1,
-      title: "Dashboard",
-      icon: MdOutlineDashboardCustomize,
-      link: PATH.DASHBOARD,
-    },
-    {
-      id: 2,
-      title: "Total Expenses",
-      icon: BsCashCoin,
-      link: PATH.EXPENSES,
-    },
-    {
-      id: 3,
-      title: "Total Income",
-      icon: BsCashCoin,
-      link: PATH.INCOME,
-    },
-    {
-      id: 4,
-      title: "Total Savings",
-      icon: BsCashCoin,
-      link: PATH.SAVINGS,
-    },
-  ];
+export const sidebar = [
+  {
+    id: 1,
+    title: "Dashboard",
+    icon: MdOutlineDashboardCustomize,
+    link: PATH.DASHBOARD,
+  },
+  {
+    id: 2,
+    title: "Total Expenses",
+    icon: BsCashCoin,
+    link: PATH.EXPENSES,
+  },
+  {
+    id: 3,
+    title: "Total Income",
+    icon: GiReceiveMoney,
+    link: PATH.INCOME,
+  },
+  {
+    id: 4,
+    title: "Total Savings",
+    icon: BsCashCoin,
+    link: PATH.SAVINGS,
+  },
+];
