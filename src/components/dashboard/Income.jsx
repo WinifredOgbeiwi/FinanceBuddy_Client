36dc36edf2c53b56bce52dc98a506d580a089da3
winifredogbeiwi <imadeogbeiwi@gmail.com>
2024-07-05 13:53:46 +0200

    income page setup

--- src/components/dashboard/Income.jsx
+++ src/components/dashboard/Income.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Income = () => {
   return (
-    <div>Income</div>
+    <main>
+      {/* header */}
+      <div className="mt-2">
+        <div className='w-full h-52 bg-main mb-2 '></div>
+        <div className='flex justify-between items-start'>
+          <div className='border-2 flex flex-col justify-center  items-center py-4 px-10'>
+            <h4>Total Balance</h4>
+            <h2>$4000</h2>
+          </div>
+          <div className='border-2 flex flex-col bg-[008000] justify-center items-center px-5 py-3'>
+            Add Income
+          </div>
+        </div>
+
+      </div>
+    </main>
   )
 }
 
